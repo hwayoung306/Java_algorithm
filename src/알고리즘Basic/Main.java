08f43366dc3d97f37e6e6c98e4d15f86a689ec5c
변화영 <hwayoung306@gmail.com>
2022-02-09 19:20:57 +0900

    알고리즘 1일차

---
+++ src/알고리즘Basic/Main.java
@@ -0,0 +1,26 @@
+package 알고리즘Basic;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int A = 0;
+		int B = 0;
+		
+		int Check = sc.nextInt();
+		if(Check>0) {
+			A = Check;
+		}
+		
+		int Check2 = sc.nextInt();
+		if(Check2 < 10) {
+			B = Check2;
+		}
+		
+		System.out.println(A*B);
+	}
+}

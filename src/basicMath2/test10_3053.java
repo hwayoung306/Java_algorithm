291bb94daf7fd9ecc5084d4fd4a12ed8e35736f9
변화영 <hwayoung306@gmail.com>
2022-04-18 19:46:27 +0900

    4/18 기본수학2

---
+++ src/basicMath2/test10_3053.java
@@ -0,0 +1,16 @@
+package basicMath2;
+
+import java.util.Scanner;
+
+public class test10_3053 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		double r = sc.nextDouble();
+		
+		System.out.println(r*r*Math.PI);
+		System.out.println(2*r*r);
+	}
+
+}
